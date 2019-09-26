bf81d7f9c3103c9993fbd79e0ea84a9e0c1386a0
pradeepch82 <pradeepch82@gmail.com>
2019-09-26 11:54:08 +0530

    Commit2

--- hello-project/src/com/pradeep/HelloWorld.java
+++ hello-project/src/com/pradeep/HelloWorld.java
@@ -4,7 +4,7 @@ public class HelloWorld {
 public static void main(String[] args) {
 	
 	for(int i=0;i<5;i++)
-		System.out.println("Welcome To Git Demo!!!");
+		System.out.println("Welcome To Git Demo!!! Commit2...");
 		
 }
 }
